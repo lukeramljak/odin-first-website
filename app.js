2e5fdce56803794518ac286cb7b6900e04f010a8
Luke Ramljak <71169775+lukeramljak@users.noreply.github.com>
2023-02-15 21:23:17 +0800

    Implement mobile nav/first section

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const navToggle = document.querySelector(".navbar-toggle");
-const links = document.querySelector(".main-nav");
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
 
-navToggle.addEventListener('click', function(){
-  links.classList.toggle("show_nav");
-})
+navToggle.addEventListener("click", () => {
+  primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded", false)
+    : navToggle.setAttribute("aria-expanded", true);
+  primaryNav.toggleAttribute("data-visible");
+}); 
